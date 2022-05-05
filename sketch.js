5f679a709bd514a28c28a94ca0595211721e0474
aditya1800@outlook.com <aditya1800@outlook.com>
2022-05-05 18:12:41 +0530

    Adding files to the repository.

--- sketch.js
+++ sketch.js
@@ -1,24 +1,37 @@
-var ball;
-
+//var ball;
+var hypotheticball, database, position ;
 function setup(){
+    database = firebase.database();
+    console.log(database);
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    hypotheticball = createSprite(250,250,10,10);
+    hypotheticball.shapeColor = "red";
+    var hypotheticballPosition = database.ref('ball/position'); 
+    hypotheticballPosition.on("value", readPosition, showError);
+
 }
 
+//database = firebase.database(); 
+//console.log(database); 
+//createCanvas(500,500); 
+//hypnoticBall = createSprite(250,250,10,10); 
+//hypnoticBall.shapeColor = "red"; 
+//var hypnoticBallPosition = database.ref('ball/position'); 
+//hypnoticBallPosition.on("value", readPosition, showError);
+//
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
@@ -26,4 +39,16 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+    
+}
+
+function readPosition(data) {
+    position = data.val(); 
+    console.log(position.x); 
+    hypotheticball.x = position.x; 
+    hypotheticball.y = position.y;
+}
+
+function showError() {
+    console.error("ERROR, Data not receieved.");
 }
